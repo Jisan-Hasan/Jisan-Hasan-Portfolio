e794442abad2f284edabdc422d1596a3c9f626c7
Jisan-Hasan <jisan.jh5@gmail.com>
2022-12-10 17:41:47 +0600

    Home page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { FaFileDownload } from "react-icons/fa";
 
 const Home = () => {
-    return <div className="bg-[#0a192f] w-full h-screen"></div>;
+    return (
+        <div name="home" className="bg-[#0a192f] w-full h-screen">
+            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+                <p className="text-2xl md:text-3xl text-yellow-300">Hi, I am</p>
+                <h1 className="text-4xl md:text-7xl font-bold text-[#ccd6f6]">Jisan Hasan</h1>
+                <h2 className="text-4xl md:text-7xl font-bold text-[#8892b0]">I'm a Full Stack Web Developer.</h2>
+                <p className="text-[#8892b0] py-4 max-w-[700px]">
+                    I'm a full stack web developer specializing in react.js. The
+                    main focus is on front-end. Current, I'm focused on learning
+                    responsive full-stack web application.
+                </p>
+                <div>
+                    <button className="flex items-center gap-1 text-white border-2 rounded-lg px-6 py-3 my-2 hover:bg-yellow-600 duration-200">
+                        Resume <FaFileDownload />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
